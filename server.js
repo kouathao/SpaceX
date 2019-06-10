98304d5057df52ad0b8ecb8184a34bd49125c3ed
Koua Thao <kouathao09@gmail.com>
2019-06-10 11:31:57 -0500

    first commit

--- server.js
+++ server.js
@@ -1,8 +1,12 @@
 const express = require("express");
 const graphqlHTTP = require("express-graphql");
+const cors = require("cors");
 const schema = require("./schema");
 
 const app = express();
+
+// Allow corss-orgin
+app.use(cors());
 
 app.use(
   "/graphql",
